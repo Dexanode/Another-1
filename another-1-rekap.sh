c6835ab89e749910976c8f53e2092871a5d1e06b
0xDexa <65535542+nadi555@users.noreply.github.com>
2022-07-04 17:55:34 +0700

    Update another-1-rekap.sh

--- another-1-rekap.sh
+++ another-1-rekap.sh
@@ -11,6 +11,8 @@ echo "█     ▐   █    ▐     ▄▀  ▄▀   ▐   ▐   █    ▐      
 echo "▐         ▐         █    ▐            ▐                 ▐         ▐        ";
 echo -e "\e[0m"
 echo -e "\033[1;31m"
+echo "Discord : XDexa#5062                                ";
+echo "Discuss Group Channel : Airdrop Sultan Indonesia    ";
 echo "Telegram : @dexa555                                ";
 echo "Twitter  : @nft_week                                ";
 echo -e "\e[0m"
